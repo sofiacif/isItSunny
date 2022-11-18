03264f652a71242b7c98dcaad4874ce2ce9f6def
Sofia Aguiar <sofia.m.aguiar@gmail.com>
2022-11-18 17:54:56 +0000

    icons change based on weather

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ let months = [
 
 let month = months[now.getMonth()];
 let day = now.getDate();
+let hour = now.getHours();
 let currentDate = document.querySelector("#current-day");
-console.log(day, month);
 currentDate.innerHTML = `${day} ${month}`;
 
 let currentCity = document.querySelector("#current-city"); //Good morning, current city
@@ -106,6 +106,69 @@ function changeCurrentGeo() {
         let geoSky = response.data.daily[0].condition.description;
 
         sky.textContent = geoSky;
+        //change icons
+        let todayIcon = document.querySelector(".today-icon");
+
+        // clear sky icon
+        if ((geoSky === `clear sky`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/clear-sky-day.svg");
+        } else if ((geoSky === `clear sky`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/clear-sky-night.svg");
+        }
+        // cloudy icon
+        if ((geoSky === `few clouds`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/cloudy-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/cloudy-night.svg");
+        }
+        // scattered clouds icon
+        if ((geoSky === `scattered clouds`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/scattered-clouds-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/scattered-clouds-night.svg");
+        }
+        // broken clouds icon
+        if ((geoSky === `broken clouds`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/broken-clouds-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/broken-clouds-night.svg");
+        }
+        // showers icon
+        if ((geoSky === `showers`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/showers-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/showers-night.svg");
+        }
+        // rain icon
+        if ((geoSky === `rain`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/rain-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/rain-night.svg");
+        }
+        // heavy intensity rain icon
+        if ((geoSky === `heavy intensity rain`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/rain-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/rain-night.svg");
+        }
+        // thunder icon
+        if ((geoSky === `thunder`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/thunder-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/thunder-night.svg");
+        }
+        // snow icon
+        if ((geoSky === `snow`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/snow-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/snow-night.svg");
+        }
+        // mist icon
+        if ((geoSky === `mist`) & (hour < 18)) {
+          todayIcon.setAttribute("src", "/icons/mist-day.svg");
+        } else if ((geoSky === `few clouds`) & (hour > 18)) {
+          todayIcon.setAttribute("src", "/icons/mist-night.svg");
+        }
       }
       axios.get(currentWeather).then(changeSky);
       //change humidity
@@ -190,17 +253,102 @@ function changeTempSearch(event) {
 
     //change sky
     function changeSky(response) {
+      event.preventDefault();
       let sky = document.querySelector("#sky");
       let geoSky = response.data.daily[0].condition.description;
-
       sky.textContent = geoSky;
+
+      //change icons and background
+      let todayIcon = document.querySelector(".today-icon");
+
+      //background colors
+      let clearSkyDay = "#FFF1B0";
+      let clearSkyNight = "0F172A";
+      // clear sky icon
+      if ((geoSky === `clear sky`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/clear-sky-day.svg");
+        document.html.body.style.backgroundColor = clearSkyDay;
+      } else if ((geoSky === `clear sky`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/clear-sky-night.svg");
+        document.html.body.style.backgroundColor = clearSkyNight;
+      }
+      // clear sky icon
+      if ((geoSky === `sky is clear`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/clear-sky-day.svg");
+        document.body.style.backgroundColor = clearSkyDay;
+      } else if ((geoSky === `sky is clear`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/clear-sky-night.svg");
+        document.html.body.style.backgroundColor = clearSkyNight;
+      }
+      // cloudy icon
+      if ((geoSky === `few clouds`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "/icons/cloudy-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/cloudy-night.svg");
+      }
+      // scattered clouds icon
+      if ((geoSky === `scattered clouds`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/scattered-clouds-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/scattered-clouds-night.svg");
+      }
+      // broken clouds icon
+      if ((geoSky === `broken clouds`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/broken-clouds-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/broken-clouds-night.svg");
+      }
+      // showers icon
+      if ((geoSky === `showers`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/showers-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/showers-night.svg");
+      }
+      // light rain icon
+      if ((geoSky === `light rain`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/showers-day.svg");
+      } else if ((geoSky === `light rain`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/showers-night.svg");
+      }
+      // rain icon
+      if ((geoSky === `rain`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/rain-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/rain-night.svg");
+      }
+
+      // heavy intensity rain icon
+      if ((geoSky === `heavy intensity rain`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/rain-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/rain-night.svg");
+      }
+      // thunder icon
+      if ((geoSky === `thunder`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/thunder-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/thunder-night.svg");
+      }
+      // snow icon
+      if ((geoSky === `snow`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/snow-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/snow-night.svg");
+      }
+      // mist icon
+      if ((geoSky === `mist`) & (hour < 18)) {
+        todayIcon.setAttribute("src", "icons/mist-day.svg");
+      } else if ((geoSky === `few clouds`) & (hour > 18)) {
+        todayIcon.setAttribute("src", "icons/mist-night.svg");
+      }
     }
+
     axios.get(cityUrl).then(changeSky);
+
     //change humidity
     function changeHumidity(response) {
       let humidity = document.querySelector("#humidity");
       let geoHumidity = response.data.daily[0].temperature.humidity;
-      console.log(geoHumidity);
       humidity.textContent = geoHumidity;
     }
     axios.get(cityUrl).then(changeHumidity);
